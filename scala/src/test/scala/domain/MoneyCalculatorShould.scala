91732d8ef8bc34e235c787e851caac1d96a146f8
Yoan Thirion <yoan.thirion@gmail.com>
2022-07-22 08:26:18 +0200

    Initialize iteration/2-portfolio

---
+++ scala/src/test/scala/domain/MoneyCalculatorShould.scala
@@ -0,0 +1,20 @@
+package domain
+
+import domain.Currency._
+import org.scalatest.funsuite.AnyFunSuite
+
+class MoneyCalculatorShould extends AnyFunSuite {
+  test("10 EUR x 2 = 20 EUR") {
+    assert(
+      MoneyCalculator
+        .times(10, EUR, 2) === 20
+    )
+  }
+
+  test("4002 KRW / 4 = 1000.5 KRW") {
+    assert(
+      MoneyCalculator
+        .divide(4002, KRW, 4) === 1000.5
+    )
+  }
+}
