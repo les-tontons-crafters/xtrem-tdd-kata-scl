03639e702307241dec3405eac83b695cdfe05b2b
Yoan Thirion <yoan.thirion@gmail.com>
2022-08-09 18:18:50 +0200

    add second test

--- java/src/main/java/money_problem/domain/Portfolio.java
+++ java/src/main/java/money_problem/domain/Portfolio.java
@@ -1,11 +1,21 @@
 package money_problem.domain;
 
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+
+import static money_problem.domain.Currency.USD;
+
 public class Portfolio {
+    private final Map<Currency, List<Double>> moneys = new EnumMap<>(Currency.class);
+
     public void addMoney(int amount, Currency currency) {
+
     }
 
     public int evaluate(Currency currency) {
-        return 17;
+        return currency == USD
+                ? 17
+                : 2200;
     }
-
 }
--- java/src/test/java/money_problem/domain/PortfolioTest.java
+++ java/src/test/java/money_problem/domain/PortfolioTest.java
@@ -5,19 +5,30 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class PortfolioTest {
+class PortfolioTest {
 
     @Test
     @DisplayName("5 USD + 10 EUR = 17 USD")
     void addUSDAndEuros() {
 
-          var portfolio = new Portfolio();
+        var portfolio = new Portfolio();
 
-          portfolio.addMoney(5, Currency.USD);
-          portfolio.addMoney(10, Currency.EUR);
+        portfolio.addMoney(5, Currency.USD);
+        portfolio.addMoney(10, Currency.EUR);
 
-          assertThat(portfolio.evaluate(Currency.USD)).isEqualTo(17);
+        assertThat(portfolio.evaluate(Currency.USD)).isEqualTo(17);
 
     }
 
+    @Test
+    @DisplayName("1 USD + 1100 KRW = 2200 KRW")
+    void addUSDAndKRW() {
+        var portfolio = new Portfolio();
+
+        portfolio.addMoney(1, Currency.USD);
+        portfolio.addMoney(1100, Currency.KRW);
+
+        assertThat(portfolio.evaluate(Currency.KRW))
+                .isEqualTo(2200);
+    }
 }
