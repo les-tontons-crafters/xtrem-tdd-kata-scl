bef5880b808e5045843c117a1d1b44a8de954cd3
HunteRoi <me@tinaeldevresse.eu>
2022-08-30 18:01:49 +0200

    make conversionresult generic

--- c#/money-problem.Domain/Portfolio.cs
+++ c#/money-problem.Domain/Portfolio.cs
@@ -16,40 +16,40 @@ public class Portfolio
         this.moneys = moneys.ToImmutableList();
     }
 
-    public Money Evaluate(Bank bank, Currency currency)
+    public Money EvaluateWithException(Bank bank, Currency currency)
     {
-        List<ConversionResult> results = this.GetConvertedMoneys(bank, currency);
+        List<ConversionResult<MissingExchangeRateException>> results = this.GetConvertedMoneys(bank, currency);
         return ContainsFailure(results)
             ? throw ToException(results)
             : ToMoney(results, currency);
     }
 
-    private static MissingExchangeRatesException ToException(IEnumerable<ConversionResult> results) =>
+    private static MissingExchangeRatesException ToException(IEnumerable<ConversionResult<MissingExchangeRateException>> results) =>
         new(results
             .Where(result => result.HasException())
             .Select(result => result.GetExceptionUnsafe())
             .ToList());
 
-    private static Money ToMoney(IEnumerable<ConversionResult> results, Currency currency) =>
+    private static Money ToMoney(IEnumerable<ConversionResult<MissingExchangeRateException>> results, Currency currency) =>
         new(results.Sum(result => result.GetMoneyUnsafe().Amount), currency);
 
-    private static bool ContainsFailure(IEnumerable<ConversionResult> results) =>
+    private static bool ContainsFailure(IEnumerable<ConversionResult<MissingExchangeRateException>> results) =>
         results.Any(result => result.HasException());
 
-    private List<ConversionResult> GetConvertedMoneys(Bank bank, Currency currency) =>
+    private List<ConversionResult<MissingExchangeRateException>> GetConvertedMoneys(Bank bank, Currency currency) =>
         this.moneys
             .Select(money => ConvertMoney(bank, currency, money))
             .ToList();
 
-    private static ConversionResult ConvertMoney(Bank bank, Currency currency, Money money)
+    private static ConversionResult<MissingExchangeRateException> ConvertMoney(Bank bank, Currency currency, Money money)
     {
         try
         {
-            return new ConversionResult(bank.Convert(money, currency));
+            return new ConversionResult<MissingExchangeRateException>(bank.Convert(money, currency));
         }
         catch (MissingExchangeRateException exception)
         {
-            return new ConversionResult(exception);
+            return new ConversionResult<MissingExchangeRateException>(exception);
         }
     }
 
@@ -60,9 +60,9 @@ public class Portfolio
         return new Portfolio(updatedMoneys);
     }
 
-    private class ConversionResult
+    public class ConversionResult<T> where T : Exception
     {
-        private readonly MissingExchangeRateException? exception;
+        private readonly T? exception;
 
         private readonly Money? money;
 
@@ -71,7 +71,7 @@ public class Portfolio
             this.money = money;
         }
 
-        public ConversionResult(MissingExchangeRateException exception)
+        public ConversionResult(T exception)
         {
             this.exception = exception;
         }
@@ -80,8 +80,21 @@ public class Portfolio
 
         public bool HasException() => this.exception != null;
 
-        public MissingExchangeRateException GetExceptionUnsafe() => this.exception!;
+        public T GetExceptionUnsafe() => this.exception!;
 
         public Money GetMoneyUnsafe() => this.money!;
+    }
+
+    public ConversionResult<MissingExchangeRatesException> Evaluate(Bank bank, Currency currency)
+    {
+        try
+        {
+            var money = this.EvaluateWithException(bank, currency);
+            return new ConversionResult<MissingExchangeRatesException>(money);
+        }
+        catch (MissingExchangeRatesException exception)
+        {
+            return new ConversionResult<MissingExchangeRatesException>(exception);
+        }
     }
 }
--- c#/money-problem.Tests/PortfolioTest.cs
+++ c#/money-problem.Tests/PortfolioTest.cs
@@ -23,38 +23,38 @@ public class PortfolioTest
     [Fact(DisplayName = "5 USD + 10 EUR = 17 USD")]
     public void Add_ShouldAddMoneyInDollarAndEuro() =>
         PortfolioWith(5d.Dollars(), 10d.Euros())
-            .Evaluate(this.bank, Currency.USD)
+            .EvaluateWithException(this.bank, Currency.USD)
             .Should()
             .Be(17d.Dollars());
 
     [Fact(DisplayName = "1 USD + 1100 KRW = 2200 KRW")]
     public void Add_ShouldAddMoneyInDollarAndKoreanWons() =>
         PortfolioWith(1d.Dollars(), 1100d.KoreanWons())
-            .Evaluate(this.bank, Currency.KRW)
+            .EvaluateWithException(this.bank, Currency.KRW)
             .Should()
             .Be(2200d.KoreanWons());
 
     [Fact(DisplayName = "5 USD + 10 EUR + 4 EUR = 21.8 USD")]
     public void Add_ShouldAddMoneyInDollarsAndMultipleAmountInEuros() =>
         PortfolioWith(5d.Dollars(), 10d.Euros(), 4d.Euros())
-            .Evaluate(bank, Currency.USD)
+            .EvaluateWithException(bank, Currency.USD)
             .Should()
             .Be(21.8.Dollars());
 
     [Fact(DisplayName = "Throws a MissingExchangeRatesException in case of missing exchange rates")]
     public void Add_ShouldThrowAMissingExchangeRatesException()
     {
-        Action act = () => PortfolioWith(1d.Euros(), 1d.Dollars(), 1d.KoreanWons())
-            .Evaluate(this.bank, Currency.EUR);
-        act.Should().Throw<MissingExchangeRatesException>()
-            .WithMessage("Missing exchange rate(s): [USD->EUR],[KRW->EUR]");
+        PortfolioWith(1d.Euros(), 1d.Dollars(), 1d.KoreanWons())
+                .Evaluate(this.bank, Currency.EUR)
+                .GetExceptionUnsafe()
+                .Message.Should().Be("Missing exchange rate(s): [USD->EUR],[KRW->EUR]");
     }
 
     [Fact(DisplayName = "5 USD + 10 USD = 15 USD")]
     public void Add_ShouldAddMoneyInTheSameCurrency()
     {
         PortfolioWith(5d.Dollars(), 10d.Dollars())
-            .Evaluate(bank, Currency.USD).Should().Be(15d.Dollars());
+            .EvaluateWithException(bank, Currency.USD).Should().Be(15d.Dollars());
     }
 
     private static Portfolio PortfolioWith(params Money[] moneys) =>
